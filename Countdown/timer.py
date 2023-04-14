c7bd3bbd44f9a476babf9383eb5863836576ef9f
hossenmaruf <severusmaruf740@gmail.com>
2023-04-14 16:43:20 +0600

    countdown

--- Countdown/timer.py
+++ Countdown/timer.py
@@ -1,5 +1,12 @@
 import time
 
-time.sleep(3)
+my_time = int(input("Enter the time in secoands: "))
 
-print("Times up")
+for x in range(my_time , 0 , -1):
+    seconds = x % 60
+    minutes = int (x/60) % 60
+
+    print(f" 00:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("TIME'S UP")
