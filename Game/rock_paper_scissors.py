40007a2fea4845fb7ab4287aa73a9db3cb93cb68
hossenmaruf <severusmaruf740@gmail.com>
2023-05-07 16:35:14 +0600

    ROCK PAPER SCISSORS GAME

--- Game/rock_paper_scissors.py
+++ Game/rock_paper_scissors.py
@@ -4,7 +4,24 @@ options = ("rock", "paper", "scissors")
 player = None
 computer = random.choice(options)
 
-player = input("Enter a choice (rock paper scissors): " )
+while player not in options:
+    player = input("Enter a choice (rock paper scissors): ")
 
 print(f"player: {player}")
 print(f"computer: {computer}")
+
+
+if player == computer :
+    print("its a tie")
+
+elif player == "rock" and computer == "scissors":
+    print("YOU WIN")
+
+elif player == "paper" and computer == "rock":
+    print("YOU WIN")
+
+elif player == "scissors" and computer == "paper":
+    print("YOU WIN")
+
+else:
+    print("YOU LOSE")
