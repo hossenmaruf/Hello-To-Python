4151a386ebe2a19e3e5863303a8fa1e043c00ff0
hossenmaruf <severusmaruf740@gmail.com>
2023-05-09 17:49:59 +0600

    Encryption and decryption

--- Encryption/Encryption.py
+++ Encryption/Encryption.py
@@ -3,14 +3,11 @@ import string
 
 chars = " " + string.punctuation + string.digits + string.ascii_letters
 
-
 chars = list(chars)
 key = chars.copy()
-
 random.shuffle(key)
 
 #Encryption
-
 user_text = input("Enter your secret text: ")
 encrypt_text = ""
 
@@ -18,6 +15,17 @@ for letter in user_text:
     index = chars.index(letter)
     encrypt_text += key[index]
 
-
 print(f"your original text : {user_text}")
 print(f"encryption text    : {encrypt_text}")
+
+#Decrypt
+
+encrypt_text = input("Enter your secret text: ")
+user_text = ""
+
+for letter in encrypt_text:
+    index = key.index(letter)
+    user_text += chars[index]
+
+print(f"encryption text    : {encrypt_text}")
+print(f"your original text : {user_text}")
