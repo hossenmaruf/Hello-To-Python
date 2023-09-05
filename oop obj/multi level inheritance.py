a05b5c57b4e1726d3d415f8e1aed26fde0abc797
hossenmaruf <severusmaruf740@gmail.com>
2023-09-06 05:16:00 +0600

    Multi-level inheritance

--- oop obj/multi level inheritance.py
+++ oop obj/multi level inheritance.py
@@ -1,17 +1,21 @@
+# multi-level inheritance = when a derived (child) class inherits another derived (child) class
+
 class Organism:
+
     alive = True
 
 class Animal(Organism):
 
     def eat(self):
-         print("animal are eating")
+        print("This animal is eating")
 
 class Dog(Animal):
+
     def bark(self):
-     print("this dog are barking")
+        print("This dog is barking")
 
 
 dog = Dog()
-print(dog.alive)
-dog.eat()
-dog.bark()
+print(dog.alive)    # inherited from the Organism class
+dog.eat()           # inherited from the Animal class
+dog.bark()          # defined in Dog class
