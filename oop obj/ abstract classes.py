46de3045283668613d656d65097534e3bd496544
hossenmaruf <severusmaruf740@gmail.com>
2023-09-07 02:36:58 +0600

    abstractmethod

---
+++ oop obj/ abstract classes.py
@@ -0,0 +1,43 @@
+# prevents a user from creating an object of that class
+# + compels a user to override abstract methods in a child class
+
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def go(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+class Car(Vehicle):
+
+    def go(self):
+        print("You drive the car")
+
+    def stop(self):
+        print("This car is stopped")
+
+class Motorcycle(Vehicle):
+
+    def go(self):
+        print("You ride the motorcycle")
+
+    def stop(self):
+        print("This motorcycle is stopped")
+
+
+#vehicle = Vehicle()
+car = Car()
+motorcycle = Motorcycle()
+
+#vehicle.go()
+car.go()
+motorcycle.go()
+
+#vehicle.stop()
+car.stop()
+motorcycle.stop()
