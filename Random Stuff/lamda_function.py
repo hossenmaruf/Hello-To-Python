0983fbe8e76b06ae9693bb6fa96108f7b5e5d5ed
hossenmaruf <severusmaruf740@gmail.com>
2023-10-12 01:39:36 +0600

    lambda function

--- Random Stuff/lamda_function.py
+++ Random Stuff/lamda_function.py
@@ -16,3 +16,6 @@ print(add(1,2,3))
 
 full_name = lambda first_name, last_name: first_name+" "+last_name
 print(full_name("hossen","maruf"))
+
+age_check = lambda age : True if age >= 18 else False
+print(age_check(16))
