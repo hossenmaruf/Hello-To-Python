878d900162a0f96213fb8f27d9d411a23c38f57a
hossenmaruf <severusmaruf740@gmail.com>
2023-10-14 03:45:19 +0600

    multitreading

--- Random Stuff/multithreading.py
+++ Random Stuff/multithreading.py
@@ -1,2 +1,27 @@
 import threading
 import time
+
+
+def eat_breakfast():
+    time.sleep(3)
+    print("You eat breakfast")
+
+
+def drink_coffee():
+    time.sleep(4)
+    print("You drank coffee")
+
+
+def study():
+    time.sleep(5)
+    print("You finish studying")
+
+
+eat_breakfast()
+drink_coffee()
+study()
+
+
+print(threading.active_count())
+print(threading.enumerate())
+print(time.perf_counter())
