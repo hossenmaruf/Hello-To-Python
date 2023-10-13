354a512f5eac6ba02a93d451d62a3547d35f7ae1
hossenmaruf <severusmaruf740@gmail.com>
2023-10-14 03:53:44 +0600

    multitreading

--- Random Stuff/multithreading.py
+++ Random Stuff/multithreading.py
@@ -1,3 +1,14 @@
+# thread =  a flow of execution. Like a separate order of instructions.
+#                  However each thread takes a turn running to achieve concurrency
+#                  GIL = (global interpreter lock),
+#                  allows only one thread to hold the control of the Python interpreter at any one time
+
+# cpu bound = program/task spends most of it's time waiting for internal events (CPU intensive)
+#             use multiprocessing
+
+# io bound = program/task spends most of it's time waiting for external events (user input, web scraping)
+#            use multithreading
+
 import threading
 import time
 
@@ -17,10 +28,18 @@ def study():
     print("You finish studying")
 
 
-eat_breakfast()
-drink_coffee()
-study()
+x = threading.Thread(target=eat_breakfast, args=())
+x.start()
 
+y = threading.Thread(target=drink_coffee, args=())
+y.start()
+
+z = threading.Thread(target=study, args=())
+z.start()
+
+x.join()
+y.join()
+z.join()
 
 print(threading.active_count())
 print(threading.enumerate())
